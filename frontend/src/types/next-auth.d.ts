fd31ddc544ad8142f176bbdbdc9faf3d161b9deb
isaias1234fer-war <g2053300921@unitru.edu.pe>
2026-07-27 11:52:35 -0500

    auth.d

---
+++ frontend/src/types/next-auth.d.ts
@@ -0,0 +1,26 @@
+import { DefaultSession } from "next-auth"
+import { JWT } from "next-auth/jwt"
+
+declare module "next-auth" {
+  interface Session {
+    accessToken?: string
+    user: {
+      id: string
+      role: string
+    } & DefaultSession["user"]
+  }
+
+  interface User {
+    accessToken?: string
+    role: string
+    id: string
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    accessToken?: string
+    role: string
+    id: string
+  }
+}
